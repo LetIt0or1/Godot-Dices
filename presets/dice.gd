051277fb0402b91a857709e316dc371514c75939
Yaroslav Movchan <akimaelio@gmail.com>
2024-03-26 20:17:45 +0100

    UI button to implement a jump was added

--- presets/dice.gd
+++ presets/dice.gd
@@ -3,7 +3,6 @@ extends Node3D
 # Variables to control the force range
 var min_force = Vector3(-10, -10, -10)
 var max_force = Vector3(10, 10, 10)
-var force_timer = Timer.new() # Creating a new timer
 
 # Assuming the RigidBody3D is the first child, or adjust the index accordingly
 var rigid_body: RigidBody3D
@@ -14,20 +13,12 @@ func _ready():
 	if not rigid_body:
 		print("RigidBody3D not found as a child. Please check the node hierarchy.")
 		return
-	
-	#_on_Timer_timeout()
-	# Adding the timer to the scene tree and configuring it
-	add_child(force_timer)
-	force_timer.wait_time = 5 # Set the timer to trigger every 5 seconds
-	force_timer.autostart = true # Start the timer automatically
-	#force_timer.connect("timeout", self, "_on_Timer_timeout") # Connecting the timeout signal to a function
-	
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		applyRandomForce()
+		print(rigid_body.position)
 
-	print(rigid_body.position)
-	
 
 func applyRandomForce():
 	# This function is called every time the timer times out (every 5 seconds in this case)
