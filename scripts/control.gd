d897907d9f6dc9e6dd4eed8a4b6fe99b5b205505
Yaroslav Movchan <akimaelio@gmail.com>
2024-04-21 00:31:35 +0200

    new build config for android + new anmated button to add/remove dices

--- scripts/control.gd
+++ scripts/control.gd
@@ -1,17 +1,8 @@
 extends Control
 
-# Main button which forces dices to rotate 
-func _on_jump_button_pressed():
-	Input.action_press("ui_accept")
-	Input.action_release("ui_accept")
-	print("_on_button_pressed")
+func _on_touch_button_remove_dice_pressed():
+	get_child(1).get_child(0).get_child(0).play("remove_dice_animation")
 
-func _on_remove_dice_button_pressed():
-	Input.action_press("ui_left")
-	Input.action_release("ui_left")
-	print("_on_remove_dice_button_pressed")
 
-func _on_add_dice_button_pressed():
-	Input.action_press("ui_right")
-	Input.action_release("ui_right")
-	print("_on_add_dice_button_pressed")
+func _on_touch_button_add_dice_pressed():
+	get_child(1).get_child(1).get_child(0).play("add_dice_animation")
